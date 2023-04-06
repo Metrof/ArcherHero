bbea6687c04fb18de52e936662908066e633ed42
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2023-04-06 23:27:01 +0300

    AddController_Alexander
    add inputController and camera

---
+++ ArcherHero/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothTime = 0.3f;
+    public Vector3 offset;
+
+    private Vector3 velocity = Vector3.zero;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        targetPosition.y = transform.position.y; 
+        targetPosition.x = transform.position.x;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
