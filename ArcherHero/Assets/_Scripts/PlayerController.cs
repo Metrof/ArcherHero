c09a3cae80ed5677350be47227b33ec20c1c7445
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2023-04-08 12:39:31 +0300

    Add InputController

---
+++ ArcherHero/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{   
+    private CharacterController controller;
+    private CharacterController Controller{get { return controller = controller ?? GetComponent<CharacterController>();} }
+  
+    private InputControls input;
+
+    private Vector2 moveDirection;
+
+    public float moveSpeed = 10f;
+
+    public void SetMoveInput(InputAction.CallbackContext context)
+    {
+        moveDirection = context.ReadValue<Vector2>();
+    }
+    
+    private void Move(Vector2 direction)
+    {
+        float scaleMoveSpeed = moveSpeed * Time.deltaTime;
+
+        Vector3 moveDirection = new Vector3(direction.x, 0, direction.y);
+        Controller.Move((moveDirection * scaleMoveSpeed) * Time.deltaTime);
+
+        if (moveDirection != Vector3.zero)
+        {
+            transform.LookAt(transform.position + moveDirection);
+        }    
+    }
+
+    private void Update()
+    {
+        Move(moveDirection);  
+    }
+}
