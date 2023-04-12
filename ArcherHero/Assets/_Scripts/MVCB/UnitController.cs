b3c5a5c98f9edfb0307743849ee3621369988c04
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2023-04-12 21:20:09 +0300

    newComponent

--- ArcherHero/Assets/_Scripts/MVCB/UnitController.cs
+++ ArcherHero/Assets/_Scripts/MVCB/UnitController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitController<V, M> 
+public class UnitController<V, M> : MonoBehaviour
     where V : UnitView 
     where M : UnitModel
 {
