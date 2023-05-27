10863411fdd7c25f965b9a5d42a91640d891f7d7
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2023-05-27 13:48:39 +0300

    Update GameManager.cs

--- ArcherHero/Assets/_Scripts/GameManager.cs
+++ ArcherHero/Assets/_Scripts/GameManager.cs
@@ -34,10 +34,13 @@ public class GameManager : MonoBehaviour
     private LvlTriggerZone _startTriggerZone;
     private LvlTriggerZone _endTriggerZone;
 
+<<<<<<< HEAD
     Dictionary<PerkManager.PerkType, PerkManager.PerkStatus> _perkStates;
 
 
 
+=======
+>>>>>>> parent of d2ab64b (path 2)
     private static int _lvlCount;
     public static int LvlCount { get { return _lvlCount; } }
 
@@ -58,6 +61,7 @@ public class GameManager : MonoBehaviour
         _perkStates = new Dictionary<PerkManager.PerkType, PerkManager.PerkStatus>();
     }
 
+<<<<<<< HEAD
 
     private void LoadPerkData()
     {
@@ -87,6 +91,8 @@ public class GameManager : MonoBehaviour
 
 
 
+=======
+>>>>>>> parent of d2ab64b (path 2)
     private void CreatePlayer(CharacterStatsE playerStats)
     {
         if (_playerPref != null)
@@ -101,12 +107,16 @@ public class GameManager : MonoBehaviour
 
             Dagger dagger = Instantiate(_dagger);
             dagger.SetOwner(_playerController.transform);
+<<<<<<< HEAD
 
 
             List<IPerk> purchasedPerks = GetPurchasedPerks();
 
             _playerController.SetFirstSkill(new Dash(2, purchasedPerks));
 
+=======
+            _playerController.SetFirstSkill(new Dash(2));
+>>>>>>> parent of d2ab64b (path 2)
             _playerController.SetSecondSkill(new Parry(dagger.gameObject, 2));
 
             _playerController.LvlStart();
