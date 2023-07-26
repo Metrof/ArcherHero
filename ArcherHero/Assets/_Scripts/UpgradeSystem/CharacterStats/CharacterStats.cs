d61eee0be2c16363994ec427a6da295db2f73e08
19-Andrey-91 <92575746+19-Andrey-91@users.noreply.github.com>
2023-07-26 15:36:10 +0300

    Fix bug with property Stats in CharacterStats

--- ArcherHero/Assets/_Scripts/UpgradeSystem/CharacterStats/CharacterStats.cs
+++ ArcherHero/Assets/_Scripts/UpgradeSystem/CharacterStats/CharacterStats.cs
@@ -12,17 +12,19 @@ namespace PlayerStats
         [SerializeField] private StatInfo _attackSpeed;
         [SerializeField] private StatInfo _movementSpeed;
 
+        private List<StatInfo> _stats;
+
         public StatInfo MaxHP => _maxHP;
         public StatInfo Damage => _damage;
         public StatInfo AttackSpeed => _attackSpeed;
         public StatInfo MovementSpeed => _movementSpeed;
 
-        public IEnumerable<StatInfo> Stats => Stats ?? new List<StatInfo>()
+        public IEnumerable<StatInfo> Stats => _stats ??= new List<StatInfo>()
         {
-            _maxHP,
-            _damage,
-            _attackSpeed,
-            _movementSpeed,
+            MaxHP,
+            Damage,
+            AttackSpeed,
+            MovementSpeed,
         };
     }
 }
