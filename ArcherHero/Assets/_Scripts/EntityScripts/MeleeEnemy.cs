a4eba3f11cec82fa68a3baf25809ee7f3607f065
AlexanderAvdeev993 <123899359+AlexanderAvdeev993@users.noreply.github.com>
2023-08-10 11:42:13 +0300

    Add MeleeEnemy

--- ArcherHero/Assets/_Scripts/EntityScripts/MeleeEnemy.cs
+++ ArcherHero/Assets/_Scripts/EntityScripts/MeleeEnemy.cs
@@ -4,8 +4,12 @@ using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AI;
 
+
 public class MeleeEnemy : Enemy
 {
+    [SerializeField] private int _timeToUpdateMoveTarget;
+    [SerializeField] private float _attackDistance;
+
     private void Start()
     {
         GameObject gObject = GameObject.FindGameObjectWithTag("Player");
@@ -23,11 +27,28 @@ public class MeleeEnemy : Enemy
         {
             var position = _targetAttack.position;
             _targetMovePosition = new Vector3(position.x, 0, position.z);
-            _agent.SetDestination(_targetMovePosition);
             
-            await UniTask.Delay(TimeSpan.FromSeconds(5), cancellationToken: _cancellationToken.Token). SuppressCancellationThrow(); 
+            _agent.SetDestination(_targetMovePosition);
+            _agent.stoppingDistance = _attackDistance;
+            
+            if (Vector3.Distance( transform.position, _targetAttack.position) <= _attackDistance)
+            {   
+                MeleeAttack();
+                await UniTask.Delay(TimeSpan.FromSeconds(AttackDelay(speedAttack)), cancellationToken: _cancellationToken.Token). SuppressCancellationThrow();
+            }
+            else
+            {
+                await UniTask.Delay(TimeSpan.FromSeconds(_timeToUpdateMoveTarget), cancellationToken: _cancellationToken.Token). SuppressCancellationThrow();
+            }
         }
     }
+    private double AttackDelay(float speedAttack)
+    {
+        return 1 / speedAttack;
+    }
 
-   
+    private void MeleeAttack()
+    {
+        TakeDamage(_typeDamage, damage);
+    }
 }
