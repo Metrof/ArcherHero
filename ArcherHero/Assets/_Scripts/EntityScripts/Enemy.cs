53c518a6833369abe0f1153089bc0cb005fe95b5
Metrof <92603982+Metrof@users.noreply.github.com>
2023-08-24 17:57:53 +0200

    fixCam

--- ArcherHero/Assets/_Scripts/EntityScripts/Enemy.cs
+++ ArcherHero/Assets/_Scripts/EntityScripts/Enemy.cs
@@ -23,12 +23,12 @@ public class Enemy : Entity
     protected override void Die()
     {
         _cancellationToken.Cancel();
-        OnEnemyDie?.Invoke(this);
         base.Die();
     }
 
     public void DestroyGO()
     {
+        OnEnemyDie?.Invoke(this);
         Destroy(gameObject);
     }
     public void DestroyEnemy()
